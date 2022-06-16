fa35ebd2b48aac7502cf14b76f4fd2b5f3ed9ca0
Akhil <kashyapakhil826@gmail.com>
2022-06-16 06:53:53 +0530

    Day-3

---
+++ LectureQuestions/Lecture-2/DataTypesAndVariables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    cout<<a<<endl;
+
+    char ch = 'a';
+    cout<<ch<<endl;
+
+    bool bl = true;
+    cout<<bl<<endl;
+
+    float f = 1.2;
+    cout<<f<<endl;
+
+    double d = 1.23;
+    cout<<d<<endl;
+
+    int size = sizeof(a);
+    cout<<size<<endl;
+}
